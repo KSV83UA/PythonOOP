086e4ae981acb4091084d069b257088099c776fa
KSV83UA <54726136+KSV83UA@users.noreply.github.com>
2023-01-18 10:59:21 +0200

    add hm02

---
+++ Homework.02.py
@@ -0,0 +1,68 @@
+# Домашнє завдання:
+# 1. Створіть клас, що описує людину (створіть метод, що виводить інформацію про людину).
+# 2) На його основі створіть клас Студент (перевизначте метод виведення інформації).
+# 3) Створіть клас Група, який містить масив із 10 об'єктів класу Студент.'
+# Реалізуйте методи додавання, видалення студента та метод пошуку студента за прізвищем.
+# Визначте для Групи метод str() для повернення списку студентів у вигляді рядка.
+
+class People:
+
+    def __init__(self, surname, name, age):
+        self.surname = surname
+        self.name = name
+        self.age = age
+    def __str__(self):
+        return f'{self.surname} {self.name[0]}'
+
+
+class Student(People):
+
+    def __init__(self,  surname, name, age, descriptions):
+        super().__init__(surname, name, age)
+        self.descriptions = descriptions
+
+    def __str__(self):
+        return f'{self.surname} {self.name[0]}.'
+
+
+class Group:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def __str__(self):
+        return '\n'.join(map(str, self.students))
+
+    def add(self, student):
+        self.students.append(student)
+
+    def remove(self, surname):
+        for i in self.students:
+            if i.surname == surname:
+                self.students.remove(i)
+                return "student remove"
+        return "student not remove"
+
+    def search(self, surname):
+        for i in self.students:
+            if i.surname == surname:
+                return i
+        return "not student"
+
+
+gr = Group("KS-00-1")
+s = {}
+for i in range(10):
+    s[i] = Student("surname" + str(i),"name" + str(i), i, "descriptions" + str(i))
+
+for i in range(10):
+    gr.add(s[i])
+
+print(gr)
+print(gr.remove("surname3"))
+print(gr)
+
+tmp = gr.search("surname4")
+
+print(tmp.name)
+
