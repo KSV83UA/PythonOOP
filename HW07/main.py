9a8efb3dc8297e0f4629ea6485938116265659c6
KSV83UA <ksv83ua@gmail.com>
2023-02-19 22:00:05 +0200

    add HW07

--- HW07/main.py
+++ HW07/main.py
@@ -1,4 +1,5 @@
 # 1. Створіть декоратор, який зареєструє декорований клас у списку.
+from time import time
 
 class_list = []
 
@@ -77,21 +78,33 @@ print("v = " + str(Box.sum_v(box_1, box_2)))
 print(x)
 
 # 4. Створіть дескриптор, який робитиме поля доступними лише для читання.
+class OrderDescriptor:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        if self.name not in instance.__dict__:
+            instance.__dict__[self.name] = value
+        pass
+
+    def __delete__(self, instance):
+        pass
+
 
 class Cart:
     def __init__(self, title, order):
         self.title = title
-        self.__first = True
         self.order = order
-    def __setattr__(self, key, value):
-        if key not in self.__dict__:
-            # setattr(self, key, value)
-            self.__dict__[key] = value
-        pass
-    def __delattr__(self, item):
-        pass
+
+    order = OrderDescriptor('order')
+
     def __str__(self):
-        return f'{self.title} {self.order}'
+        return str(self.order)
+
+
 
 x_1 = Cart('order', 10)
 x_1.order = 20
@@ -105,20 +118,70 @@ class Number:
 
     def __setattr__(self, key, value):
         if not isinstance(value, int):
-            #raise TypeError специально закоментировал, чтобы небыло исключения
-            pass
-        else:
-            self.__dict__[key] = value
+            raise TypeError
+        self.__dict__[key] = value
+
     def __str__(self):
         return str(self.value)
 
 x_2 = Number(10)
 print(x_2)
 x_2.value = 12
-x_2.value = "asdasd"
-print((x_2))
+# x_2.value = "asdasd"
+print("integer=", (x_2))
+
 # 6. Реалізуйте властивість класу, яка має наступний функціонал: при установці значення цієї властивості у файл із
 # заздалегідь визначеним ім'ям повинен зберігатися час (коли встановлювали значення властивості) та встановлене значення.
+class Cls1:
+    def __init__(self, value):
+        self.file_name = "cls1.txt"
+        self.value = value
+
+    @property
+    def value(self):
+        return self.__value
+
+    @value.setter
+    def value(self, value):
+        with open(self.file_name, "a") as fls:
+            fls.write(f'{time()} : value={value}\n')
+        self.__value = value
+
+    def __str__(self):
+        return str(self.value)
+
+    @value.deleter
+    def value(self):
+        pass
+
+
+x_3 = Cls1(10)
+x_3.value = 20
+print(f'x_3: {x_3}')
+
 # 7. Створіть ABC клас із абстрактним методом перевірки цілого числа на простоту. Тобто якщо параметром цього методу є
 # ціле число і воно просте, то метод повинен повернути True, а в іншому випадку False. Створіть похідний клас. Перевірте
 # працездатність проекту.
+
+from abc import ABC, abstractmethod
+
+class IsSimple(ABC):
+    @abstractmethod
+    def is_simple(self, number):
+        pass
+
+class CheckNumber(IsSimple):
+    def __init__(self):
+        pass
+
+    def is_simple(self, number):
+        if not isinstance(number, int):
+            return False
+        for i in range(2, (number // 2) + 1):
+            if not number % i:
+                return False
+        return True
+
+
+x_4 = CheckNumber()
+print(x_4.is_simple(11))
